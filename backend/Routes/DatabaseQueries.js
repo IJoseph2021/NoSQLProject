26082b5db1a2adb4ad762a5c43f06926b16ddd04
unknown <ijoseph@smu.edu>
2021-12-04 16:43:11 -0600

    changed

--- backend/Routes/DatabaseQueries.js
+++ backend/Routes/DatabaseQueries.js
@@ -17,14 +17,25 @@ let authors = db.collection("authors");
 let papers = db.collection("papers");
 
 async function add_author(first_name, last_name) {
-  let document = {
-    "first_name": first_name,
-    "last_name": last_name,
-    "employment": []
-  };
   await client.connect();
-  await authors.insertOne(document);
-  client.close();
+  let author = await authors.findOne({
+    $and: [
+      { "first_name": first_name },
+      { "last_name" : last_name  }
+    ]
+  });
+  if (author) {
+    return author._id;
+  }
+  else {
+    let document = {
+      "first_name": first_name,
+      "last_name": last_name,
+      "employment": []
+    };
+    let id = await authors.insertOne(document);
+    return id;
+  }
 }
 
 async function add_employment_to_author(first_name, last_name, employment_name, employment_start, employment_end){
@@ -45,13 +56,12 @@ async function add_employment_to_author(first_name, last_name, employment_name, 
   };
   await client.connect();
   await authors.findOneAndUpdate(query, update);
-  client.close();
+  ;
 }
 
 async function add_paper(
   title,
-  author_first_names,
-  author_last_names,
+  author_ids,
   publication_name,
   publication_journal,
   publication_number,
@@ -61,33 +71,6 @@ async function add_paper(
   page_number
 ){
   await client.connect();
-  let author_ids = [];
-  console.log(title,
-    author_first_names,
-    author_last_names,
-    publication_name,
-    publication_journal,
-    publication_number,
-    publication_year,
-    publication_location,
-    url,
-    page_number)
-  for (var i = 0; i < author_first_names.length; i++) {
-    var author = await authors.findOne({
-      $and: [
-        { "first_name": author_first_names[i] },
-        { "last_name" : author_last_names[i]  }
-      ]
-    });
-    
-    if (author) {
-      author_ids.push(author._id);
-      
-    }
-    else {
-      return i;
-    }
-  }
   let document = {
     "title": title,
     "authors": author_ids,
@@ -102,21 +85,30 @@ async function add_paper(
     "page_number": page_number
   }
   await papers.insertOne(document);
-  client.close();
-  return -1;
 }
 
 async function get_all_papers() {
+  
   await client.connect();
   let documents = await papers.find({}).toArray();
-  client.close();
+  for (var i = 0; i < documents.length; i++) {
+    for (var j = 0; j < documents[i].authors.length; j++) {
+      var id = documents[i].authors[j];
+      var obj = await authors.findOne(id);
+      documents[i].authors[j] = obj.first_name + " " + obj.last_name;
+    }
+  }
   return documents;
 }
 
 async function get_paper_by_title(title) {
   await client.connect();
   let document = await papers.findOne({ "title": title });
-  client.close();
+  let ids = document.authors;
+  for (var i = 0; i < ids.length; i++) {
+    var obj = await authors.findOne(ids[i]);
+    document.authors[i] = obj.first_name + " " + obj.last_name;
+  }
   return document;
 }
 
@@ -128,9 +120,15 @@ async function get_papers_by_author(first_name, last_name) {
       { "last_name" : last_name  }
     ]
   });
-  let documents = await papers.find({ "authors": author._id });
-  client.close();
-  return documents.toArray();
+  let documents = await papers.find({ "authors": author._id }).toArray();
+  for (var i = 0; i < documents.length; i++) {
+    for (var j = 0; j < documents[i].authors.length; j++) {
+      var id = documents[i].authors[j];
+      var obj = await authors.findOne(id);
+      documents[i].authors[j] = obj.first_name + " " + obj.last_name;
+    }
+  }
+  return documents;
 }
 
 async function get_papers_by_publication_years(publisher, range_start, range_end) {
@@ -141,9 +139,15 @@ async function get_papers_by_publication_years(publisher, range_start, range_end
       { "publication.year": { $gte: range_start } },
       { "publication.year": { $lte: range_end } }
     ]
-  });
-  client.close()
-  return documents.toArray();
+  }).toArray();
+  for (var i = 0; i < documents.length; i++) {
+    for (var j = 0; j < documents[i].authors.length; j++) {
+      var id = documents[i].authors[j];
+      var obj = await authors.findOne(id);
+      documents[i].authors[j] = obj.first_name + " " + obj.last_name;
+    }
+  }
+  return documents;
 }
 
 async function get_coauthors(first_name, last_name) {
@@ -154,14 +158,14 @@ async function get_coauthors(first_name, last_name) {
       { "last_name" : last_name  }
     ]
   });
-  const coauthor0_papers = await papers.find({ "authors": author._id });
-  const coauthor0_ids = [].concat(coauthor0_papers.toArray().map(a => a.authors));
-  const coauthor1_papers = await papers.find({ "authors": { $in: coauthor0_ids }});
-  const coauthor1_ids = [].concat(coauthor1_papers.toArray().map(a => a.authors));
-  const coauthor2_papers = await papers.find({ "authors": { $in: coauthor1_ids }});
-  const coauthor2_ids = [].concat(coauthor2_papers.toArray().map(a => a.authors));
-  const coauthor3_papers = await papers.find({ "authors": { $in: coauthor2_ids }});
-  const coauthor3_ids = [].concat(coauthor3_papers.toArray().map(a => a.authors));
+  const coauthor0_papers = await papers.find({ "authors": author._id }).toArray();
+  const coauthor0_ids = [].concat(coauthor0_papers.map(a => a.authors));
+  const coauthor1_papers = await papers.find({ "authors": { $in: coauthor0_ids }}).toArray();
+  const coauthor1_ids = [].concat(coauthor1_papers.map(a => a.authors));
+  const coauthor2_papers = await papers.find({ "authors": { $in: coauthor1_ids }}).toArray();
+  const coauthor2_ids = [].concat(coauthor2_papers.map(a => a.authors));
+  const coauthor3_papers = await papers.find({ "authors": { $in: coauthor2_ids }}).toArray();
+  const coauthor3_ids = [].concat(coauthor3_papers.map(a => a.authors));
   let coauthors_3 = [];
   for (var i = 0; i < coauthor3_ids.length; ++i) {
     var object = await authors.findOne({ "_id": coauthor3_ids[i] }); 
--- backend/Routes/papers.js
+++ backend/Routes/papers.js
@@ -3,15 +3,15 @@ let Paper = require('../models/papers.model');
 const { get_all_papers, get_paper_by_title, get_papers_by_author, get_papers_by_publication_years, add_paper } = require('./DatabaseQueries');
 
 router.route('/getPapers').get((req, res) => {
-  res.send(get_all_papers().then(res=>console.log(res)));
+  get_all_papers().then(value => {res.send(value)});
 });
 
 router.route('/getPaper/:title').get((req, res) => {
-  res.send(get_paper_by_title(req.params["title"]).then(res=>console.log(res)));
+  get_paper_by_title(req.params["title"]).then(value => {res.send(value)});
 });
 
 router.route('/getPaper/:first/:last').get((req, res) => {
-  res.send(get_papers_by_author(req.params["first"], req.params["last"]).then(res=>console.log(res)));
+  get_papers_by_author(req.params["first"], req.params["last"]).then(value => {res.send(value)});
 });
 
 router.route('/getPaper/:publisher/:start/:end').get((req, res) => {
@@ -25,8 +25,7 @@ router.route('/getPaper/:publisher/:start/:end').get((req, res) => {
 router.route('/addPaper').put((req, res) => {
   res.send(add_paper(
     req.body["title"],
-    req.body["author_first_names"],
-    req.body["author_last_names"],
+    req.body["author_ids"],
     req.body["publication_name"],
     req.body["publication_journal"],
     req.body["publication_number"],
