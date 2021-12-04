3b8d319a061568bda4397846e8265d2eb0bda707
unknown <ijoseph@smu.edu>
2021-12-03 23:29:45 -0600

    dog

--- frontend/src/api/accountRepository.js
+++ frontend/src/api/accountRepository.js
@@ -1,11 +1,15 @@
 import { MongoClient } from 'mongodb';
-import { username, password } from './credentials.js';
+import { username, password, cluster_url } from './credentials.js';
 
-const uri = `mongodb+srv://${username}:${password}@nosqldb.b2v2m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${username}:${password}@${cluster_url}`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("sample_airbnb").collection("listingsAndReviews");
-  var document = collection.findOne( { _id: "10006546" } );
-  console.log(document);
+async function test() {
+  await client.connect();
+  const db = client.db("sample_airbnb");
+  const collection = db.collection("listingsAndReviews");
+  const document = await collection.findOne( { _id: "10006546" } );
+  console.log(document.listing_url);
   client.close();
-});
+};
+
+await test();
