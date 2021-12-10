da19f97d6ece370c7f84ef03e31416b94423e4f5
unknown <ijoseph@smu.edu>
2021-12-10 17:31:32 -0600

    final

--- backend/Routes/authors.js
+++ backend/Routes/authors.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 let Author = require('../models/authors.model');
-const { get_coauthors, add_author, add_employment_to_author } = require('./DatabaseQueries');
+const { get_coauthors, add_author, add_employment_to_author, get_employment_by_author } = require('./DatabaseQueries');
 
 router.route('/getCoauthors/:first/:last').get((req, res) => {
   get_coauthors(req.params["first"], req.params["last"]).then(value => {res.send(value)})
@@ -18,6 +18,11 @@ router.route('/addEmployment/:first/:last').put((req, res) => {
     req.body["employment_start"],
     req.body["employment_end"]
   ).then(value => {res.send(value)}));
+});
+
+
+router.route('/getEmployment/:first/:last').get((req, res) => {
+  get_employment_by_author(req.params["first"], req.params["last"]).then(value => {res.send(value)})
 });
 
 module.exports = router;
